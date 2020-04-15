cadbb8e80a99e2131ddf86d1971ad522bec2e4e7
go.dog <969295028@qq.com>
2020-04-15 13:38:35 +0800

    新增登陆鉴权服务

---
+++ app/user-web/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+        log "github.com/micro/go-micro/v2/logger"
+	      "net/http"
+        "github.com/micro/go-micro/v2/web"
+        "cs/app/user-web/handler"
+)
+
+func main() {
+	// create new web service
+        service := web.NewService(
+                web.Name("go.micro.cs.web.user"),
+                web.Version("latest"),
+        )
+
+	// initialise service
+        if err := service.Init(); err != nil {
+                log.Fatal(err)
+        }
+
+	// register html handler
+	service.Handle("/", http.FileServer(http.Dir("html")))
+
+	// register call handler
+	service.HandleFunc("/user/call", handler.UserCall)
+
+	// run service
+        if err := service.Run(); err != nil {
+                log.Fatal(err)
+        }
+}
