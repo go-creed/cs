7b52abaef04015daf8ff93453e28053024c2b393
go.dog <969295028@qq.com>
2020-06-03 14:34:21 +0800

    1. 调整【auth-srv】包，使用etcd作为配置中心

--- app/auth-srv/main.go
+++ app/auth-srv/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"os"
+	"flag"
 	"time"
 
 	"github.com/micro/cli/v2"
@@ -10,32 +10,49 @@ import (
 	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/registry/etcd"
 
+	"cs/app/auth-srv/conf"
 	"cs/app/auth-srv/handler"
 	"cs/app/auth-srv/model"
 	auth "cs/app/auth-srv/proto/auth"
 	"cs/plugin/rd"
-	_const "cs/public/const"
+	"cs/public/config"
 )
 
+var (
+	configCenter = *flag.String("cc", "127.0.0.1:2379", "")
+	etcdCfg      config.EtcdConfig
+)
+
+func initCfg() {
+	flag.Parse()
+	var err error
+	config.Init(configCenter, conf.Init)
+	etcdCfg, err = config.ETCD()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	// Init Config
+	initCfg()
 	// Registry by etcd
 	// 用读取环境变量的方式来做
-	os.Getenv("")
 	etcdRegistry := etcd.NewRegistry(
-		registry.Addrs("127.0.0.1:2379"),
-		registry.Timeout(time.Second*5),
+		registry.Addrs(etcdCfg.Addrs),
+		registry.Timeout(time.Duration(etcdCfg.Timeout)),
 	)
 	// New Service
 	service := micro.NewService(
-		micro.Name(_const.AuthSrv),
-		micro.Version("latest"),
+		micro.Name(conf.Config().Name),
+		micro.Version(conf.Config().Version),
 		micro.Registry(etcdRegistry),
-		micro.Address("127.0.0.1:12004"),
+		micro.Address(conf.Config().Address),
 	)
 
 	// Initialise service
 	service.Init(
-		micro.Action(func(context *cli.Context) error {
+		micro.Action(func(c *cli.Context) error {
 			// Init Redis
 			rd.Init()
 			// Init Model
